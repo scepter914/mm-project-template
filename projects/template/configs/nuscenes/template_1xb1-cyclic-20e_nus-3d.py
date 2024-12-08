1f16c0c0e45ca661c3008a929961c719364920c4
scepter914 <scepter914@gmail.com>
2024-12-08 18:28:14 +0900

    apply format
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- projects/template/configs/nuscenes/template_1xb1-cyclic-20e_nus-3d.py
+++ projects/template/configs/nuscenes/template_1xb1-cyclic-20e_nus-3d.py
@@ -1,6 +1,7 @@
-_base_ = ['../../../../configs/_base_/default_runtime.py']
+_base_ = ["../../../../configs/_base_/default_runtime.py"]
 custom_imports = dict(
-    imports=['projects.example_project.dummy'], allow_failed_imports=False)
+    imports=["projects.example_project.dummy"], allow_failed_imports=False
+)
 
 
-_base_.model.backbone.type = 'DummyResNet'
+_base_.model.backbone.type = "DummyResNet"
